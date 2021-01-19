7aacb4df83ad6b3d41d9899717407fadb9cc1bfb
JamesVZD <dimitrisverlekis@gmail.com>
2021-01-19 22:18:33 +0200

    Ministry Done

--- ministry/people.php
+++ ministry/people.php
@@ -3,20 +3,20 @@ include_once 'header-footer/header.php' // Include header of page
 ?>
 
 <head>
-  <title>Υπηρεσίες | Υπουργείο Εργασίας & Κοινωνικών Υποθέσεων</title>
+  <title>Πολιτική Διοίκηση | Υπουργείο Εργασίας & Κοινωνικών Υποθέσεων</title>
 </head>
 
 
 <div class="bgded" style="background:white;">
   <div id="breadcrumb" class="hoc clear">
-    <h6 class="heading font-x3">Υπηρεσίες</h6>
+    <h6 class="heading font-x3">Πολιτική Διοίκηση</h6>
     <ul>
       <li><a style="color: #1c7aa8;" class="hyperlink" href="../index.php">ΑΡΧΙΚΗ</a></li>
       <li><span>&#47;</span> </li>
       <li><a style="color: #1c7aa8;" class="hyperlink" href="ministry.php">ΥΠΟΥΡΓΕΙΟ</a></li>
       <li><span>&#47;</span> </li>
       <li>
-        <p style="color: #363636;">Υπηρεσιες</p>
+        <p style="color: #363636;">Πολιτικη Διοικηση</p>
       </li>
     </ul>
   </div>
@@ -26,43 +26,21 @@ include_once 'header-footer/header.php' // Include header of page
   <main class="hoc mycontainer clear">
     <div class="content covid">
       <div class="group btmspace-50">
-        <h6 style="margin-top:0px;" class="heading">Κεντρικές Υπηρεσίες</h6>
+        <h6 style="margin-top:0px;" class="heading">Υπουργός</h6>
         <ul>
-          <p>Η κεντρική διοίκηση του υπουργείου, υπέυθυνη για το σύνολο των δραστηριοτήτων του.
-            Μπορείτε να απευθυνθείτε εδώ για οργανωτικά, εργατικά, διοικητικά και εκτελεστικά ζητήματα.</p>
-          <ul>
-            <p><i class="fa fa-phone"></i> 213-1516649</p>
-            <p><i class="fa fa-envelope-o"></i> pliroforisi-politi@ypakp.gr</p>
-          </ul>
+          <p>Κωστής Χατζηδάκης</p>
         </ul>
-        <h6 class="heading top-margin-50">Γενική Γραμματεία Κοινωνικών Ασφαλίσεων</h6>
+        <h6 class="heading top-margin-50">Υφυπουργός για Θέματα Κοινωνικής Ασφάλισης</h6>
         <ul>
-          <p>Ο υπεύθυνος φορέας για το σύνολο των ασφαλιστικών ζητημάτων Μπορείτε να απευθυνθείτε εδώ για θέματα εισφορών, ενσήμων, χρεών, καθώς και οποιαδήποτε εκκρεμότητα με ασφαλιστικά ταμεία (ΕΚΦΑ/ΙΚΑ/ΟΑΕΕ).
-          </p>
-          <ul>
-            <p><i class="fa fa-phone"></i> 213-1523000</p>
-            <p><i class="fa fa-envelope-o"></i> ggka@ypakp.gr</p>
-          </ul>
+          <p>Παναγιώτης Τσακάλογλου</p>
         </ul>
-        <h6 class="heading top-margin-50">Σώμα Επιθεώρησης Εργασίας</h6>
+        <h6 class="heading top-margin-50">Υφυπουργός για Θέματα Πρόνοιας & Κοινωνικής Αλληλεγγύης</h6>
         <ul>
-          <p>Ο υπέυθυνος φορέας για την τήρηση του εργατικού δικαίου και της κείμενης νομοθεσίας.
-            Μπορείτε να απευθυνθείτε εδώ για καταγγελίες περί εργασιακής νομοθεσίας, φορολογικών παραβάσεων καθώς και τη μή τήρηση των εν ισχύ μέτρων για την ανάσχεση της πανδημίας.
-          </p>
-          <ul>
-            <p><i class="fa fa-phone"></i> 213-1577500</p>
-            <p><i class="fa fa-envelope-o"></i> sepe@ypakp.gr</p>
-          </ul>
+          <p>Δόμνα Μιχαηλίδου</p>
         </ul>
-        <h6 class="heading top-margin-50">Γενική Γραμματεία Πρόνοιας</h6>
+        <h6 class="heading top-margin-50">Υφυπουργός Δημογραφιικής Πολιτικής & Οικογένειας</h6>
         <ul>
-          <p>Ο υπεύθυνος φορέας για την ισότητα και την προσφορά στο σύνολο των εργαζομένων.
-            Μπορείτε να απευθυνθείτε εδώ για επιδόματα, σίτιση, εργασιακά δικαιώματα και άλλα ζητήματα πρόνοιας.
-          </p>
-          <ul>
-            <p><i class="fa fa-phone"></i> 213-1588300</p>
-            <p><i class="fa fa-envelope-o"></i> ggpro@ypakp.gr</p>
-          </ul>
+          <p>Μαρία Συρέγγελα</p>
         </ul>
       </div>
     </div>
